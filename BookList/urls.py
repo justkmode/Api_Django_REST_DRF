9a91521518f7dbf07922687ecdaff105fdd3927d
justkmode <136920389+justkmode@users.noreply.github.com>
2025-01-19 11:40:16 +0100

    Update urls.py

--- BookList/urls.py
+++ BookList/urls.py
@@ -1,11 +1,8 @@
 # BookList/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
-from BookListAPI import views  # Make sure to import the views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('BookListAPI.urls')),  # Include BookListAPI URLs under '/api/'
-    path('', views.index, name='index'),  # Maps root URL to BookListAPI's index view
 ]
