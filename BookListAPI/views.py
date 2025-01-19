0635372b8b4b360e5cf6c00afad31415139eac34
justkmode <136920389+justkmode@users.noreply.github.com>
2025-01-19 11:40:21 +0100

    Update views.py

--- BookListAPI/views.py
+++ BookListAPI/views.py
@@ -1,16 +1,11 @@
-# BookListAPI/views.py
-
-from django.http import HttpResponse
+# views.py
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-# Using the @api_view decorator
 @api_view(['GET', 'POST'])
 def books(request):
     if request.method == 'GET':
-        # Here you can retrieve books from the database or just return a sample response
-        return Response({"message": "List of books"})
-
+        return Response({"message": "List of books as JSON"})
     elif request.method == 'POST':
-        # Here you can handle book creation (for simplicity, returning a dummy response)
-        return Response({"message": "Book created successfully!"}, status=201)
+        # Handle the POST request and add book logic
+        return Response({"message": "Book added"})
